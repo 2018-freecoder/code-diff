e54e36a79cf054d0d9ba97a00ddbc84c0301c97d
chaomao <6233843@gmail.com>
2019-10-11 21:57:31 +0800

    add other comment

--- diff.sh
+++ diff.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
- pushd home-ideas
- git pull
+# pushd home-ideas
+ #git pull
  old_version=`cat ../version.txt`
  echo $old_version
- git log -1 --format=format:"%H" > ../version.txt
+ #git log -1 --format=format:"%H" > ../version.txt
  git diff $old_version..head | mate
  popd
