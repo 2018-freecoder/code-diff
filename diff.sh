187cc59fef4f6c9871c0c9bf786d472d28b4321b
chaomao <6233843@gmail.com>
2019-10-11 21:53:37 +0800

    add some comment

--- diff.sh
+++ diff.sh
@@ -3,6 +3,7 @@
  git pull
  old_version=`cat ../version.txt`
  echo $old_version
+# add some comment f1
  git log -1 --format=format:"%H" > ../version.txt
  git diff $old_version..head | mate
  popd
